30dd960634e3e6436ca4e3f526519370c71576cb
Shukra Tamang <shukratamang.npl@gmail.com>
2025-03-16 15:31:41 +1100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center pt-10 font-bold">
+        react-firebase-serverless-app
+      </h1>
+    </div>
+  );
+};
+
+export default App;
